0154fe1ff7fb49aaa6d2973024322d2924ab04c1
Axieof <pritheev24@gmail.com>
2025-03-28 16:08:06 +0800

    Moved to BTO folder

--- Applicant.java
+++
@@ -1,9 +0,0 @@
-package BTOSystem;
-
-public class Applicant extends User {
-
-    // Constructor for Applicant
-    public Applicant (String name, String nric, int age, String maritalStatus) {
-        super(name, nric, age, maritalStatus);
-    }
-}
--- HDBManager.java
+++
@@ -1,8 +0,0 @@
-package BTOSystem;
-
-public class HDBManager extends User {
-    // Constructor for Applicant
-    public HDBManager (String name, String nric, int age, String maritalStatus, String password) {
-        super(name, nric, age, maritalStatus, password);
-    }
-}
