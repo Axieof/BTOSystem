0d1251bd1d44fb282da632f05022606800687f59
Axieof <pritheev24@gmail.com>
2025-03-28 14:40:45 +0800

    Added Constructor

--- HDBOfficer.java
+++ HDBOfficer.java
@@ -1,7 +1,10 @@
 package BTOSystem;
 
-public class HDBOfficer {
-    public static void main(String[] args) {
-
+public class HDBOfficer extends Applicant {
+    
+    // Constructor for HDB Officer
+    public HDBOfficer(String name, String nric, int age, String maritalStatus) {
+        super(name, nric, age, maritalStatus);
     }
+    
 }
