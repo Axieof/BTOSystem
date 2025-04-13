081e6654bdb903e26b0f0832c02a0e1e3e5d959a
iptcyq <somefishystuffz@gmail.com>
2025-04-13 20:58:37 +0800

    Updated Enquiry and ApplicantView
    The bulk of Enquiry should be complete, Added to ApplicantView template. (Yet to implement relevant classes for filter, EnquiryView etc)

--- BTO/Enquiry.java
+++ BTO/Enquiry.java
@@ -1,5 +1,34 @@
 package BTO;
 
 public class Enquiry {
-
+	private String question;
+	private String reply;
+	
+	private Applicant sender;
+	private User respondent;
+	
+	private boolean answered;
+	
+	public Enquiry(String q, Applicant send) {
+		question = q;
+		sender = send;
+		answered = false;
+	}
+	
+	public String getEnquiry() { return question; }
+	public Applicant getSender() { return sender; }
+	
+	public String getResponse() { return reply; }
+	public User getRespondent() { return respondent; }
+	public boolean isAnswered() { return answered; }
+	
+	public void editEnquiry(String edited) { 
+		if (!answered) question = edited;
+		else System.out.println("ERROR: Enquiry closed. Unable to edit.");
+	}
+	public void respondEnquiry(String repl, User resp) {
+		reply = repl;
+		respondent = resp;
+		answered = true;
+	}
 }
--- View/ApplicantView.java
+++ View/ApplicantView.java
@@ -1,12 +1,28 @@
 package View;
+import BTO.*;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ApplicantView implements ILandingPageView{
+	// General attributes
+	private ArrayList<ProjectListing> projects = new ArrayList<ProjectListing>();
+	private Applicant applicant;
+	
+	// Tools
+	Scanner sc = new Scanner(System.in);
+	
+	// Constructor
+	public ApplicantView(Applicant appl, ArrayList<ProjectListing> projs){
+		applicant = appl;
+		projects = projs;
+	}
+	
 	public void viewLandingPage() {
-		Scanner sc = new Scanner(System.in);
-		int choice = sc.nextInt();
+		int choice = getChoice();
+		System.out.println();
 		
-		while (choice != -1) {
+		while (choice != 0) {
 			switch (choice) {
 			case 1-> viewProjects();
 			case 2-> applyProject();
@@ -14,26 +30,89 @@ public class ApplicantView implements ILandingPageView{
 			case 4-> requestAppWithdrawal();
 			case 5-> handleEnquiry();
 			}
+			
+			choice = getChoice();
+			System.out.println();
+		}
+		
+		// SOMETHING HERE TO DEAL WITH EXITING VIEW
+	}
+	
+	private int getChoice() {
+		int choice = -1;
+
+		System.out.println("What would you like to do?\n"
+							+ "1. View projects\n"
+							+ "2. Apply for project\n"
+							+ "3. View your applied project\n"
+							+ "4. Request application withdrawal\n"
+							+ "5. Handle your enquiries\n"
+							+ "0. EXIT/ LOGOUT IDK");
+		choice = sc.nextInt();
+		
+		if (0 <= choice && choice <= 5) return choice;
+		else {
+			System.out.println("ERROR: Invalid choice.");
+			return -1;
 		}
 	}
 	
 	public void viewProjects() {
+		System.out.println("List of projects:");
 		
+		// FILTER PROJECTS INTO filteredProjs
+		
+		for (ProjectListing proj : projects) {
+			if (proj.getVisibility()) {
+				System.out.println(proj);
+				System.out.println();
+			}
+		}
 	}
 	
 	public void applyProject() {
+		System.out.println("Applying for project...");
+
+		System.out.println("Which would you like to apply for?");
 		
+		// FILTER PROJECTS INTO filteredProjs
+		
+		ProjectListing proj;
+		for (int i=0; i<projects.size(); i++) {
+			proj = projects.get(i);
+			if (proj.getVisibility()) {
+				System.out.println(i + ". " + proj.getProjectName());
+			}
+		}
+		
+		int choice = sc.nextInt();
+		proj = projects.get(choice);
+		
+		ArrayList<Unit> allUnits = proj.getUnitTypes();
+		// THEN CHOOSE UNIT TYPE (FILTER)
+		// CREATE AND SEND APPLICATION
 	}
 	
 	public void viewAppliedProject() {
-		
+		// IF PROJECTAPPLICATION EXISTS
+		// GET PROJECTAPPLICATION FROM APPLICANT
+		// THEN VIEW
+		// ELSE RETURN HAVE NOT APPLIED FOR PROJECT
 	}
 	
 	public void requestAppWithdrawal() {
-		
+		// IF PROJECTAPPLICATION EXISTS
+		// GET PROJECTAPPLICATION FROM APPLICANT
+		// SEND WITHDRAWAL REQUEST
+		// ELSE RETURN HAVE NOT APPLIED FOR PROJECT
 	}
 	
 	public void handleEnquiry() {
-		
+		// Possible things to do
+		// SEND ENQUIRY
+		// EDIT ENQUIRY
+		// DELETE ENQUIRY
+		// GET RESPONSE TO ENQURIY
+		// MIGHT NEED FILTER TOO (answered/ unanswered)
 	}
 }
