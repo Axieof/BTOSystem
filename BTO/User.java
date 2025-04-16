3f2e15e937950bdefa34bbdcffb6486d1339a5e7
R3Q4 <reneeteoruining@gmail.com>
2025-04-16 18:20:50 +0800

    Updated getter and setters for User class

--- BTO/User.java
+++ BTO/User.java
@@ -22,6 +22,19 @@ public class User {
     public User() {
 
     }
+    // getter
+    public String getName() {return name;}
+    public String getNric() {return nric;}
+    public int getAge() {return age;}
+    public String getMaritalStatus() {return maritalStatus;}
+    public String getPassword() {return password;}
+    public String getRole() {return role;}
+    
+    // setters
+    public void setPassword(String newPassword) {this.password = newPassword;}
+    public void setRole(String role) {this.role = role;}
+    public void setDefaultPassword() {this.password = "password";}
+
 
     // Password changing method
     public boolean changePassword(String oldPassword, String newPassword) {
