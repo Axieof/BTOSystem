bbe1a1dae9d4bc5683fd1937e9c4898c2d4d93aa
Axieof <pritheev24@gmail.com>
2025-04-22 00:45:20 +0800

    Applicant MVC

---
+++ src/com/BTO/Controller/ApplicantController.java
@@ -0,0 +1,10 @@
+package src.com.BTO.Controller;
+
+public class ApplicantController {
+    
+    private Applicant applicant;
+
+    public ApplicantController(Applicant applicant) {
+        this.applicant = applicant;
+    }
+}
---
+++ src/com/BTO/View/ApplicantView.java
@@ -0,0 +1,9 @@
+package src.com.BTO.View;
+
+public class ApplicantView {
+    
+    // TODO 
+    // public void printApplicantDetails(String applicantName) {
+    //     System.out.println(...)
+    // }
+}
