8120136f352434ad786ce43f2497061a2d649f37
Axieof <pritheev24@gmail.com>
2025-04-22 00:53:56 +0800

    Extends Applicant

--- src/com/BTO/Model/HDBOfficer.java
+++ src/com/BTO/Model/HDBOfficer.java
@@ -4,7 +4,7 @@ import src.com.BTO.Model.Enums.MaritalStatus;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HDBOfficer extends User {
+public class HDBOfficer extends Applicant {
 
     public HDBOfficer(String name, String nric, int age, MaritalStatus maritalStatus, String password) {
         super(name, nric, age, maritalStatus, password);
