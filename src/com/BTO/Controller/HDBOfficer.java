a5aa798e76ade97c7e28fcdab8f912e8d381e69b
Axieof <pritheev24@gmail.com>
2025-04-22 00:53:11 +0800

    Added HDBOfficer Template

---
+++ src/com/BTO/Controller/HDBOfficer.java
@@ -0,0 +1,10 @@
+package src.com.BTO.Controller;
+
+public class HDBOfficer {
+    private HDBOfficer hdbOfficer;
+
+    public HDBOfficerController(HDBOfficer hdbOfficer) {
+        this.hdbOfficer = hdbOfficer;
+    }
+}
+}
--- src/com/BTO/Model/HDBOfficer.java
+++ src/com/BTO/Model/HDBOfficer.java
@@ -4,10 +4,9 @@ import src.com.BTO.Model.Enums.MaritalStatus;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HDBManager extends User {
+public class HDBOfficer extends User {
 
-    public HDBManager(String name, String nric, int age, MaritalStatus maritalStatus, String password) {
+    public HDBOfficer(String name, String nric, int age, MaritalStatus maritalStatus, String password) {
         super(name, nric, age, maritalStatus, password);
     }
-    
 }
