588e98773bfaa52eab0d44e04d562b5cdf53abf7
Axieof <pritheev24@gmail.com>
2025-04-23 03:49:09 +0800

    Fixed HDBOfficerController

---
+++ src/com/BTO/Controller/HDBOfficerController.java
@@ -0,0 +1,13 @@
+package src.com.BTO.Controller;
+
+import src.com.BTO.Model.HDBOfficer;
+
+public class HDBOfficerController {
+    
+    private HDBOfficer hdbOfficer;
+
+    public HDBOfficerController(HDBOfficer hdbOfficer) {
+        this.hdbOfficer = hdbOfficer;
+    }
+}
+}
