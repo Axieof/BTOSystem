1bef2ec3c907dc26baa9578e3af55dc5638f6339
iptcyq <somefishystuffz@gmail.com>
2025-04-22 18:00:51 +0800

    Added changePassword in UserSettingsController

--- src/com/BTO/Controller/UserSettingsController.java
+++ src/com/BTO/Controller/UserSettingsController.java
@@ -1,15 +1,18 @@
 package src.com.BTO.Controller;
 
-import src.com.BTO.Model.Applicant;
+import java.util.Scanner;
+
+import src.com.BTO.Model.User;
 import src.com.BTO.Service.MenuInputService;
 import src.com.BTO.View.UserView;
 
 public class UserSettingsController  {
 	
-	private Applicant applicant;
+	private User user;
+	private Scanner sc = new Scanner(System.in);
 	
-	public UserSettingsController(Applicant appl) {
-    	applicant = appl;
+	public UserSettingsController(User u) {
+		user = u;
     }
 	
 	public void viewLandingPage() {
@@ -17,7 +20,7 @@ public class UserSettingsController  {
     	
     	while (choice != 0) {
     		switch (choice) {
-    		case 1-> UserView.displayUser(applicant);
+    		case 1-> UserView.displayUser(user);
     		case 2-> changePassword();
     		case 3-> editFilter();
     		}
@@ -26,11 +29,27 @@ public class UserSettingsController  {
     		choice = MenuInputService.getMenuInput();
     	}
     	
-    	System.out.println("Exiting user settings...");
+    	System.out.println("Exiting user settings...\n");
 	}
 	
 	private void changePassword() {
+		System.out.println("Changing password...");
+		System.out.println("Enter current password to confirm:");
 		
+		String password = sc.next();
+		boolean correct = user.checkPassword(password);
+		
+		if (correct) {
+			System.out.println("Enter new password:");
+			password = sc.next();
+			user.changePassword(password);
+			System.out.println("Password changed!\n");
+			// SHOULD SAVE UPDATED TO CSV FILE
+		}
+		else {
+			System.out.println("ERROR: Incorrect password!");
+			System.out.println("Terminating...\n");
+		}
 	}
 	
 	private void editFilter() {
