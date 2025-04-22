6cc6401a42883af391ab4fd38f7c5e2c886b69d7
iptcyq <somefishystuffz@gmail.com>
2025-04-22 14:46:40 +0800

    Replaced original files into Enums and Model

---
+++ src/com/BTO/Model/Enquiry.java
@@ -0,0 +1,37 @@
+package src.com.BTO.Model;
+
+import src.com.BTO.Model.Applicant;
+
+public class Enquiry {
+	// variables
+	private int enquiryID; 
+
+	private String message;
+	private String reply;
+	
+	private Applicant applicantName;
+	private User respondent;
+	private Applicant projectName;
+	
+	//need date and time?
+
+	private boolean answered;
+	
+	public Enquiry(String message, Applicant applicantName) {
+		this.message = message;
+		this.applicantName = applicantName;
+		this.answered = false;
+	}
+	
+	// getters
+	public String getEnquiry() { return message; }
+	public Applicant getSender() { return applicantName; }
+	
+	public String getResponse() { return reply; }
+	public User getRespondent() { return respondent; }
+
+
+	public boolean isAnswered() { return answered; }
+	
+
+}
---
+++ src/com/BTO/Model/Enums/ApplicationStatus.java
@@ -0,0 +1,8 @@
+package src.com.BTO.Model.Enums;
+
+public enum ApplicationStatus{
+	PENDING,
+	SUCCESSFUL,
+	UNSUCCESSFUL,
+	BOOKED
+}
